29c1faf4d54087dc093c4798190db0ad568445a4
vmeng <jmeng2007@163.com>
2013-05-16 16:19:10 +0800

    2 divs navigation

--- vikiTest/demo.js
+++ vikiTest/demo.js
@@ -3,6 +3,7 @@ var applicationViewModel = function() {
 	self = this;
 	
 	self.showContentDiv = function(data,event){
+		
 		location.href = "#content";
 	}
 }
@@ -11,11 +12,20 @@ var explanationViewModel = function() {
 	self = this;
 	
 	self.segmentItemJson = ko.observableArray();
-
+	self.backToNav = function(){
+		location.href="";
+	}
 	self.backgroundImageUrl = function(data) {
 
 		var imageUrl = 'url(/assets/' + data.name + '.gif)';
 
 		return imageUrl;
 	}
+}
+
+var pagerViewModel = {
+	
+	application:new applicationViewModel(),
+	explanation:new explanationViewModel()
+	
 }
