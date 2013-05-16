cc6803e915cae6cb1a91e3ec764826f9c5e31467
vmeng <jmeng2007@163.com>
2013-05-16 12:18:31 +0800

    add div, can work ,still have bugs

--- vikiTest/demo.js
+++ vikiTest/demo.js
@@ -1,4 +1,21 @@
 
 var applicationViewModel = function() {
+	self = this;
 	
+	self.showContentDiv = function(data,event){
+		location.href = "#content";
+	}
+}
+
+var explanationViewModel = function() {
+	self = this;
+	
+	self.segmentItemJson = ko.observableArray();
+
+	self.backgroundImageUrl = function(data) {
+
+		var imageUrl = 'url(/assets/' + data.name + '.gif)';
+
+		return imageUrl;
+	}
 }
